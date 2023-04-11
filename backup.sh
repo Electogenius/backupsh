501ff44eaa882d819b4bce36bd0f03b0c100a6d4
Electogenius <thirty-nine-hundred@pm.me>
2023-04-11 13:17:40 +0530

    yeh

--- backup.sh
+++ backup.sh
@@ -1,13 +1,25 @@
 # Backup
 #	Simple shell script to make a quick backup of a folder
-#
 
-NAME="backup"
+# Usage:
+#	sh backup.sh source_dir destination_parent_folder_that_already_exists [optional_backup_name]
+
+
 if [ $# = 3 ]; then
 	NAME="backup_$3"
+else
+	NAME="backup"
 fi
-echo "\n\n\tMaking a backup of $1 to $2 which will be named $NAME\n\tPress any key to continue or ctrl-c to abort"
+
+C="\033[0;1;36m"
+N="\033[0m"
+
+echo "\n\n\tMaking a backup of $C$1$N to $C$2$N under the sub-folder $C$NAME$N
+\tPress any key to continue or ctrl-c to abort"
 read -s -n1 key
+
 rm -rf $2/$NAME
-cp -a $1 $2/$NAME
-echo "\n\n\tBackup complete. Files saved to $2/$NAME\n"
+cp -a $1 $2/$NAME &&
+echo "\n\n\tBackup complete. Files saved to $C$2/$NAME$N\n"
+
+date >> $2/$NAME/backup-date.txt
