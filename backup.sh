6797ee7009c47a969a0ab7eac0df2a2a5ecdae4b
Electogenius <thirty-nine-hundred@pm.me>
2023-04-12 16:54:52 +0530

    unnecessary polish oh and did i mention RSYNC

--- backup.sh
+++ backup.sh
@@ -2,7 +2,7 @@
 #	Simple shell script to make a quick backup of a folder
 
 # Usage:
-#	sh backup.sh source_dir destination_parent_folder_that_already_exists [optional_backup_name]
+#	sh backup.sh source_dir destination_parent_dir [optional_backup_name]
 
 
 if [ $# = 3 ]; then
@@ -15,11 +15,14 @@ C="\033[0;1;36m"
 N="\033[0m"
 
 echo "\n\n\tMaking a backup of $C$1$N to $C$2$N under the sub-folder $C$NAME$N
-\tPress any key to continue or ctrl-c to abort"
+\t${C}Press any key$N to begin or ctrl-c to abort"
 read -s -n1 key
 
 rm -rf $2/$NAME
-cp -a $1 $2/$NAME &&
-echo "\n\n\tBackup complete. Files saved to $C$2/$NAME$N\n"
+{
+	rsync -avzhP --stats $1 $2/$NAME &&
+	echo "\n\n\tBackup complete. Files saved to $C$2/$NAME$N\n" &&
+	date >> $2/$NAME/backup-date.txt
+} ||
 
-date >> $2/$NAME/backup-date.txt
+echo "\n\n\t${C}Oh gosh$N\n\tSomething went horribly wrong.\n"
